0dcfe450aacf7acd79d832a325f8c7b18f38c3a3
DarrenHuang0411 <102675368+DarrenHuang0411@users.noreply.github.com>
2022-11-23 22:10:17 +0800

    Add files via upload

--- DivideNum.v
+++ DivideNum.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Create Date: 2022/11/20 11:39:46
+// Create Date:
 // Design Name: 
 // Module Name: DivideNum
 //////////////////////////////////////////////////////////////////////////////////
 module DivideNum #(parameter N = 16)(
 input clk,
-input rstn,
+input reset,
 input enable,
 input [15:0]dividend, //³Q°£¼??
 input [15:0]divisor,  //°£¼??
@@ -19,19 +19,26 @@ reg [1:0] state;
 reg [1:0] state_next;
 reg [31:0] dividend_next;
 reg [15:0] remainder_next;
-reg count;//??��?�為16
+reg count;//??��?�為16
 
 //idle   ==> 2'd0
 //shift  ==> 2'd1
 //done ==> 2'd2
 
-always @ (posedge clk or negedge rstn)
+always @ (posedge clk or posedge reset)
 begin 
-    if(!rstn)
+    if(reset)
     begin
         count <= N;
         state <= 2'd0;
-    end
+        ///////////////////////////
+        quotient <= 16'b0;
+        remainder <=16'b0;
+        dividend_next <= 32'b0;
+        remainder_next <= 16'b0;
+        checkflag = 0;
+        ///////////////////////////
+    end 
     else
     begin
         state <= state_next;
@@ -42,27 +49,26 @@ begin
     end
 end
 
-always @ (*)
+always @ (posedge clk or posedge enable)
 begin 
     case(state)
-        3'd0:
+        2'd0:
         begin
-            dividend_next <= 32'b0;
             checkflag <= 1'b0;
             if(enable)
-                begin
-                    dividend_next <= {(16'b0),dividend};
-                    state_next <= 2'd1;  
-                end
+            begin
+                dividend_next <= {(16'b0),dividend};
+                state_next <= 2'd1;  
+            end
             else
-                begin  
-                    state_next <= 2'd0;
-                end
+            begin  
+                state_next <= 2'd0;
+            end
         end
-        3'd1://shift
+        2'd1://shift
         begin
             checkflag = 1'b0; 
-            dividend_next <= (dividend_next << 1);
+            dividend_next <= {dividend_next[30:0],1'b0};
             if(dividend_next[31:16] >= divisor)
             begin
                 dividend_next[0] <= 1'b1;
@@ -73,16 +79,16 @@ begin
                 dividend_next <= dividend_next;
             end
             if (count == 1)
-                state_next = 2'd2;
+                state_next <= 2'd2;
             else
-                state_next = 2'd1;             
+                state_next <= 2'd1;             
         end
-        3'd2://done
+        2'd2://done
         begin 
             quotient <= dividend_next[15:0];
             remainder <= dividend_next[31:16];
             checkflag = 1'b1;
-            state_next <= 2'd0;         
+            state_next <= 2'd0;      
         end
         default:
         begin
@@ -90,6 +96,5 @@ begin
             state_next <= 2'd0; 
         end
     endcase
-
 end
 endmodule
